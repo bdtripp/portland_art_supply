53abff603e5a7f785e0f38c4634f16174a1e26c0
Brian Tripp <brian.tripp@pcc.edu>
2020-06-23 13:29:20 -0700

    Added GA Global Site tag to each page

--- subcategories.php
+++ subcategories.php
@@ -31,6 +31,15 @@ $categoryName = $_GET[PRODUCT_CATEGORY_NAME_FIELD];
 <html lang="en">
 
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-135450898-2"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-135450898-2');
+    </script>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
