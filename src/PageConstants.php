5686dd75acd85ba9a2269c1beea3b773ea8644fe
Brian Tripp <btripp365@gmail.com>
2026-05-23 11:58:47 -0500

    refactor: migrate PageConstants to namespaced class

--- src/PageConstants.php
+++ src/PageConstants.php
@@ -1,185 +1,183 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Brian
- * Date: 10/22/2018
- * Time: 7:18 PM
- */
+Namespace PAS;
 
-const DOMAIN_NAME = 'http://localhost';
+class PageConstants
+{
+    const DOMAIN_NAME = 'http://localhost';
 
-const HOME_PAGE = '/';
-const ABOUT_PAGE = '/about.php';
-const LOGIN_PAGE = '/login.php';
-const LOGOUT_PAGE = '/logout.php';
-const CREATE_ACCOUNT_PAGE = '/create_account.php';
-const SHOPPING_CART_PAGE = '/shopping_cart.php';
-const SUBCATEGORIES_PAGE = '/subcategories.php';
-const GROUP_PRODUCTS_PAGE = '/product_groups.php';
-const PRODUCT_ITEMS_PAGE = '/product_items.php';
+    const HOME_PAGE = '/';
+    const ABOUT_PAGE = '/about.php';
+    const LOGIN_PAGE = '/login.php';
+    const LOGOUT_PAGE = '/logout.php';
+    const CREATE_ACCOUNT_PAGE = '/create_account.php';
+    const SHOPPING_CART_PAGE = '/shopping_cart.php';
+    const SUBCATEGORIES_PAGE = '/subcategories.php';
+    const GROUP_PRODUCTS_PAGE = '/product_groups.php';
+    const PRODUCT_ITEMS_PAGE = '/product_items.php';
 
-/*
- * Page Titles
- */
+    /*
+    * Page Titles
+    */
 
-const HOME_PAGE_TITLE = 'Home';
-const SHOPPING_CART_PAGE_TITLE = 'Cart';
-const ABOUT_PAGE_TITLE = 'About';
+    const HOME_PAGE_TITLE = 'Home';
+    const SHOPPING_CART_PAGE_TITLE = 'Cart';
+    const ABOUT_PAGE_TITLE = 'About';
 
-/*
- * Code Files
- */
-const DB_CODE = 'db_code.php';
+    /*
+    * Code Files
+    */
+    const DB_CODE = 'db_code.php';
 
-/*
- * Folders
- */
+    /*
+    * Folders
+    */
 
-const IMAGE_FOLDER = 'images/';
-const SOCIAL_FOLDER = 'Social/';
+    const IMAGE_FOLDER = 'images/';
+    const SOCIAL_FOLDER = 'Social/';
 
-/*
- * Images and alt text
- */
+    /*
+    * Images and alt text
+    */
 
-const SHOPPING_CART_IMAGE = 'shopping_cart.png';
-const SHOPPING_CART_IMAGE_ALT = 'Shopping Cart Icon';
-const FACEBOOK_ICON = 'facebook.png';
-const FACEBOOK_ICON_ALT = 'Facebook Icon';
-const X_ICON = 'x.png';
-const X_ICON_ALT = 'X Icon';
-const INSTAGRAM_ICON = 'instagram.png';
-const INSTAGRAM_ICON_ALT = 'Instagram Icon';
-const PINTEREST_ICON = 'pinterest.png';
-const PINTEREST_ICON_ALT = 'Pinterest Icon';
+    const SHOPPING_CART_IMAGE = 'shopping_cart.png';
+    const SHOPPING_CART_IMAGE_ALT = 'Shopping Cart Icon';
+    const FACEBOOK_ICON = 'facebook.png';
+    const FACEBOOK_ICON_ALT = 'Facebook Icon';
+    const X_ICON = 'x.png';
+    const X_ICON_ALT = 'X Icon';
+    const INSTAGRAM_ICON = 'instagram.png';
+    const INSTAGRAM_ICON_ALT = 'Instagram Icon';
+    const PINTEREST_ICON = 'pinterest.png';
+    const PINTEREST_ICON_ALT = 'Pinterest Icon';
 
-/*
- * IDs
- */
+    /*
+    * IDs
+    */
 
-const PRODUCT_ITEM_IMAGE_ID = 'product_item_image';
-const LOGIN_LINK_ID = 'login_link';
-const CART_COUNT_DISPLAY_ID = 'cart_count_display';
-const TOTAL_DISPLAY_ID = 'total_display';
-const ITEM_DETAILS_DIV = 'item_details';
-const ITEM_OPTIONS_DIV = 'item_options';
-const ITEM_OPTIONS_RIGHT_COL = 'item_options_right_col';
-const GROUP_INFORMATION_ID = 'group_info';
-const PRODUCT_GROUPS_ID ='product_groups';
-const CHECKOUT_BUTTON_ID = 'checkout_button';
-const LOGIN_ICON_ID = 'login_icon';
-const LOGOUT_ICON_ID = 'logout_icon';
-const USERNAME_DISPLAY_ID = 'username_display';
-const WRAP_PAS = 'wrap_pas';
-const NO_WRAP_PAS = 'no_wrap_pas';
-const ITEM_WRAPPER_ID = 'item_wrapper';
-const IMAGE_WRAPPER_ID = 'image_wrapper';
-const DETAILS_WRAPPER_ID = 'details_wrapper';
-const MOBILE_H2 = 'mobile_version';
-const NON_MOBILE_H2 = 'non_mobile_version';
-const COLOR_DIV_ID = 'color';
-const SIZE_DIV_ID = 'size';
-const QUANTITY_DIV_ID = 'quantity';
-const PASSWORD_MESSAGE_ID = 'password_message';
-const USERNAME_MESSAGE_ID = 'username_message';
-const CONFIRM_PASSWORD_MESSAGE_ID = 'confirm_password_message';
-const USERNAME_INPUT_ID = 'username_input';
-const PASSWORD_INPUT_ID = 'password_input';
-const CONFIRM_PASSWORD_INPUT_ID = 'confirm_password_input';
-const LINKS_CLASS = 'links';
-const ERROR_MESSAGE_CONTAINER = 'error_message_container';
-const COLOR_THUMBNAILS_WRAPPER_ID = 'color_thumbnails_wrapper';
-const DROP_DOWN_WRAPPER_ID = 'drop_down_wrapper';
-const ABOUT_TEXT_ID = 'about_text';
-const ABOUT_IMAGE_DIV_ID = 'about_images';
-const ABOUT_SECTION_ID = 'about';
-const COLOR_THUMBNAILS_DIV_ID = 'color_thumbnails';
-const EMPTY_CART_MESSAGE = 'empty_cart_message';
-const CARD_CLASS = 'card';
-const X_ID = 'x';
-const FACEBOOK_ID = 'facebook';
-const INSTAGRAM_ID = 'instagram';
-const PINTEREST_ID = 'pinterest';
-const CREATE_ACCOUNT_LINK_ID = "create_account_link";
-const HOME_LINK_ID = "home_link";
-const UPPERCASE_REQUIREMENT_ID = "uppercase_requirement";
-const DIGIT_REQUIREMENT_ID = "digit_requirement";
-const SPECIAL_CHAR_REQUIREMENT_ID = "special_char_requirement";
-const LENGTH_REQUIREMENT_ID = "length_requirement";
-const LOGIN_BUTTON_ID = "login_btn";
+    const PRODUCT_ITEM_IMAGE_ID = 'product_item_image';
+    const LOGIN_LINK_ID = 'login_link';
+    const CART_COUNT_DISPLAY_ID = 'cart_count_display';
+    const TOTAL_DISPLAY_ID = 'total_display';
+    const ITEM_DETAILS_DIV = 'item_details';
+    const ITEM_OPTIONS_DIV = 'item_options';
+    const ITEM_OPTIONS_RIGHT_COL = 'item_options_right_col';
+    const GROUP_INFORMATION_ID = 'group_info';
+    const PRODUCT_GROUPS_ID ='product_groups';
+    const CHECKOUT_BUTTON_ID = 'checkout_button';
+    const LOGIN_ICON_ID = 'login_icon';
+    const LOGOUT_ICON_ID = 'logout_icon';
+    const USERNAME_DISPLAY_ID = 'username_display';
+    const WRAP_PAS = 'wrap_pas';
+    const NO_WRAP_PAS = 'no_wrap_pas';
+    const ITEM_WRAPPER_ID = 'item_wrapper';
+    const IMAGE_WRAPPER_ID = 'image_wrapper';
+    const DETAILS_WRAPPER_ID = 'details_wrapper';
+    const MOBILE_H2 = 'mobile_version';
+    const NON_MOBILE_H2 = 'non_mobile_version';
+    const COLOR_DIV_ID = 'color';
+    const SIZE_DIV_ID = 'size';
+    const QUANTITY_DIV_ID = 'quantity';
+    const PASSWORD_MESSAGE_ID = 'password_message';
+    const USERNAME_MESSAGE_ID = 'username_message';
+    const CONFIRM_PASSWORD_MESSAGE_ID = 'confirm_password_message';
+    const USERNAME_INPUT_ID = 'username_input';
+    const PASSWORD_INPUT_ID = 'password_input';
+    const CONFIRM_PASSWORD_INPUT_ID = 'confirm_password_input';
+    const LINKS_CLASS = 'links';
+    const ERROR_MESSAGE_CONTAINER = 'error_message_container';
+    const COLOR_THUMBNAILS_WRAPPER_ID = 'color_thumbnails_wrapper';
+    const DROP_DOWN_WRAPPER_ID = 'drop_down_wrapper';
+    const ABOUT_TEXT_ID = 'about_text';
+    const ABOUT_IMAGE_DIV_ID = 'about_images';
+    const ABOUT_SECTION_ID = 'about';
+    const COLOR_THUMBNAILS_DIV_ID = 'color_thumbnails';
+    const EMPTY_CART_MESSAGE = 'empty_cart_message';
+    const CARD_CLASS = 'card';
+    const X_ID = 'x';
+    const FACEBOOK_ID = 'facebook';
+    const INSTAGRAM_ID = 'instagram';
+    const PINTEREST_ID = 'pinterest';
+    const CREATE_ACCOUNT_LINK_ID = "create_account_link";
+    const HOME_LINK_ID = "home_link";
+    const UPPERCASE_REQUIREMENT_ID = "uppercase_requirement";
+    const DIGIT_REQUIREMENT_ID = "digit_requirement";
+    const SPECIAL_CHAR_REQUIREMENT_ID = "special_char_requirement";
+    const LENGTH_REQUIREMENT_ID = "length_requirement";
+    const LOGIN_BUTTON_ID = "login_btn";
 
 
-/*
- * Classes
- */
+    /*
+    * Classes
+    */
 
-const SUB_INTRO_CLASS = 'sub_intro';
-const SUBCATEGORIES_CLASS = 'sub_cats';
-const MENU_CLASS = 'menu';
-const MENU_BUTTON_CLASS = 'menu-btn';
-const MENU_ICON_CLASS = 'menu-icon';
-const NAVIGATION_ICON_CLASS = 'navicon';
-const ACTIVE_CLASS = 'active';
-const CLEAR_FLOAT_CLASS = 'clearfloat';
-const TWO_COLUMNS_CLASS = 'two columns';
-const THREE_COLUMNS_CLASS = 'three columns';
-const FOUR_COLUMNS_CLASS = 'four columns';
-const FIVE_COLUMNS_CLASS = 'five columns';
-const SIX_COLUMNS_CLASS = 'six columns';
-const SEVEN_COLUMNS_CLASS = 'seven columns';
-const EIGHT_COLUMNS_CLASS = 'eight columns';
-const NINE_COLUMNS_CLASS = 'nine columns';
-const TEN_COLUMNS_CLASS = "ten columns";
-const HOURS_CLASS = 'hours';
-const ADDRESS_CLASS = 'address';
-const SOCIAL_CLASS = 'social';
-const PHONE_MOBILE_CLASS = 'phone_mobile';
-const PHONE_NOT_MOBILE_CLASS = 'phone_not_mobile';
-const ICON_CLASS = 'icon';
-const LARGE_IMAGE_CLASS = 'lg_image';
-const HEADER_LINKS_CLASS = 'header_links';
-const DARK_BACKGROUND_CLASS = 'dark_background';
-const CART_ITEMS_WRAPPER_ID = "cart_items_wrapper";
-const CART_ITEM_CLASS = 'cart_item';
-const SUBTOTAL_DISPLAY_CLASS = 'subtotal_display';
-const GROUP_DESCRIPTION_TEXT_CLASS = "group_description_text";
-const PRODUCT_GROUP_CLASS = 'product_group';
-const CART_ITEM_INFO_CLASS = 'cart_item_info';
-const CART_ITEM_SPECS_CLASS = 'cart_item_specs';
-const QUANTITY_DIV_CLASS = 'quantity';
-const PRICE_DISPLAY_CLASS = 'price_display';
-const QUANTITY_AND_SUBTOTAL_CLASS = 'quantity_and_subtotal';
-const REMOVE_BUTTON_CLASS = 'remove_button';
-const SHOPPING_CART_ICON_CLASS = 'shopping_cart_icon';
-const SUBTOTAL_CLASS = 'subtotal';
-const LARGE_H2 = 'large_h2';
-const SUBCATEGORY_BUTTON_CLASS = 'subcategory_button';
-const NAV_DROPDOWN_CLASS = 'nav_drop_down';
-const DROPDOWN_CONTENT_CLASS = 'dropdown_content';
-const MESSAGE_CLASS = 'message';
-const REQUIREMENTS_CLASS = 'requirements';
-const MEETS_REQUIREMENTS_CLASS = 'meets_requirements';
-const STILL_NEEDED_CLASS = 'still_needed';
-const COLOR_THUMBNAIL_CLASS = 'color_tn';
-const WRAPPER_CLASS = "wrapper";
-const PASSWORD_SECTION_CLASS = "password_section";
-const ERROR_SYMBOL_CLASS = "error_symbol";
-const MESSAGE_WRAPPER_CLASS = "message_wrapper";
+    const SUB_INTRO_CLASS = 'sub_intro';
+    const SUBCATEGORIES_CLASS = 'sub_cats';
+    const MENU_CLASS = 'menu';
+    const MENU_BUTTON_CLASS = 'menu-btn';
+    const MENU_ICON_CLASS = 'menu-icon';
+    const NAVIGATION_ICON_CLASS = 'navicon';
+    const ACTIVE_CLASS = 'active';
+    const CLEAR_FLOAT_CLASS = 'clearfloat';
+    const TWO_COLUMNS_CLASS = 'two columns';
+    const THREE_COLUMNS_CLASS = 'three columns';
+    const FOUR_COLUMNS_CLASS = 'four columns';
+    const FIVE_COLUMNS_CLASS = 'five columns';
+    const SIX_COLUMNS_CLASS = 'six columns';
+    const SEVEN_COLUMNS_CLASS = 'seven columns';
+    const EIGHT_COLUMNS_CLASS = 'eight columns';
+    const NINE_COLUMNS_CLASS = 'nine columns';
+    const TEN_COLUMNS_CLASS = "ten columns";
+    const HOURS_CLASS = 'hours';
+    const ADDRESS_CLASS = 'address';
+    const SOCIAL_CLASS = 'social';
+    const PHONE_MOBILE_CLASS = 'phone_mobile';
+    const PHONE_NOT_MOBILE_CLASS = 'phone_not_mobile';
+    const ICON_CLASS = 'icon';
+    const LARGE_IMAGE_CLASS = 'lg_image';
+    const HEADER_LINKS_CLASS = 'header_links';
+    const DARK_BACKGROUND_CLASS = 'dark_background';
+    const CART_ITEMS_WRAPPER_ID = "cart_items_wrapper";
+    const CART_ITEM_CLASS = 'cart_item';
+    const SUBTOTAL_DISPLAY_CLASS = 'subtotal_display';
+    const GROUP_DESCRIPTION_TEXT_CLASS = "group_description_text";
+    const PRODUCT_GROUP_CLASS = 'product_group';
+    const CART_ITEM_INFO_CLASS = 'cart_item_info';
+    const CART_ITEM_SPECS_CLASS = 'cart_item_specs';
+    const QUANTITY_DIV_CLASS = 'quantity';
+    const PRICE_DISPLAY_CLASS = 'price_display';
+    const QUANTITY_AND_SUBTOTAL_CLASS = 'quantity_and_subtotal';
+    const REMOVE_BUTTON_CLASS = 'remove_button';
+    const SHOPPING_CART_ICON_CLASS = 'shopping_cart_icon';
+    const SUBTOTAL_CLASS = 'subtotal';
+    const LARGE_H2 = 'large_h2';
+    const SUBCATEGORY_BUTTON_CLASS = 'subcategory_button';
+    const NAV_DROPDOWN_CLASS = 'nav_drop_down';
+    const DROPDOWN_CONTENT_CLASS = 'dropdown_content';
+    const MESSAGE_CLASS = 'message';
+    const REQUIREMENTS_CLASS = 'requirements';
+    const MEETS_REQUIREMENTS_CLASS = 'meets_requirements';
+    const STILL_NEEDED_CLASS = 'still_needed';
+    const COLOR_THUMBNAIL_CLASS = 'color_tn';
+    const WRAPPER_CLASS = "wrapper";
+    const PASSWORD_SECTION_CLASS = "password_section";
+    const ERROR_SYMBOL_CLASS = "error_symbol";
+    const MESSAGE_WRAPPER_CLASS = "message_wrapper";
 
-/*
- * Social Media Sites
- */
+    /*
+    * Social Media Sites
+    */
 
-const FACEBOOK_PAGE_URL = 'https://www.facebook.com/';
-const TWITTER_PAGE_URL = 'https://x.com/';
-const INSTAGRAM_PAGE_URL = 'https://www.instagram.com/';
-const PINTEREST_PAGE_URL = 'https://www.pinterest.com/';
+    const FACEBOOK_PAGE_URL = 'https://www.facebook.com/';
+    const TWITTER_PAGE_URL = 'https://x.com/';
+    const INSTAGRAM_PAGE_URL = 'https://www.instagram.com/';
+    const PINTEREST_PAGE_URL = 'https://www.pinterest.com/';
 
-/*
- * Session keys
- */
+    /*
+    * Session keys
+    */
 
-const SESSION_USER_ID_KEY = 'userID';
-const SESSION_USERNAME_KEY = 'username';
-const SESSION_CART_KEY = 'cart';
-const SESSION_RETURN_TO_URL = 'returnToURL';
+    const SESSION_USER_ID_KEY = 'userID';
+    const SESSION_USERNAME_KEY = 'username';
+    const SESSION_CART_KEY = 'cart';
+    const SESSION_RETURN_TO_URL = 'returnToURL';
+}
