559b5a94a14a38cf9a069db68c1f73a2c1e9f0b9
Brian Tripp <btripp365@gmail.com>
2026-05-26 14:36:33 -0500

    refactor: move dotenv to config.php

--- config.php
+++ config.php
@@ -1,3 +1,7 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
-define('PROJECT_ROOT', __DIR__);
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
--- src/PAS/Database.php
+++ src/PAS/Database.php
@@ -1,8 +1,8 @@
 <?php
 namespace PAS;
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
+use PDO;
+use PDOException;
 
 class Database
 {
